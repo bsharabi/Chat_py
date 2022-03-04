c9bc720f941c572de88874197f264d3b45c52035
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-03-04 17:13:43 +0200

    up controller

--- client/Controller.py
+++ client/Controller.py
@@ -1,12 +1,11 @@
-import threading
-from Client import *
-from GUI import GUI_Panel
+import sys
+sys.path.append("./Client")
 
+from BLL.Client import *
+from UIL.GUI import GUI_Panel
 
 if __name__=="__main__":
     client= Client()
     gui= GUI_Panel(client)
     gui()
-    print("hellllllo")
    
-    pass
--- server/controller.py
+++ server/controller.py
@@ -1,6 +1,9 @@
-import db 
+from DAL.DataBase import * 
+from UIL.View import GUI_Panel
+from BLL.TCP_server import TCPServer
 
 if __name__=="__main__":
-    
-    
-    pass
+    db=mongodb("Chat-py")
+    server= TCPServer(db)
+    gui= GUI_Panel(server)
+    gui()
