7fcd401f9f60a1117658f6f5ea2e781fff842a43
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-03-06 14:45:03 +0200

    new Test

--- Test/Test_Client.py
+++ Test/Test_Client.py
@@ -1,30 +1,116 @@
+import sys
+sys.path.append(".")
+
+sys.path.append("Client")
+sys.path.append("Server")
+import shutil
+import time
 import unittest
+from os import path
+import os
+from Server.BLL.TCP_server import TCPServer
+from Client.BLL.Client import Client
+from Server.DAL.DataBase import mongodb
+import threading
+import time
+import randomname
 
 class TestClient(unittest.TestCase):
 
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName=methodName)
+        self.client = Client(name="barak")
         
     def test_connect_to_TCP(self):
-        pass
+        ans = self.client.connect_to_TCP()
+        self.assertEqual(ans, False)
+        db=mongodb("Chat-py")
+        serverTest = TCPServer(db)
+        serverTest.connect_to_TCP()
+        threading.Thread(target=serverTest).start()
+        time.sleep(2)
+        ans = self.client.connect_to_TCP()
+        self.assertTrue(ans)
+        serverTest.close_connection_TCP()
+
+    def test_connect_to_UDP(self):
+        ans = self.client.connect_to_UDP()
+        self.assertTrue(ans)
+
+    def test_close_TCP(self):
+        db=mongodb("Chat-py")
+        serverTest = TCPServer(db)
+        serverTest.connect_to_TCP()
+        threading.Thread(target=serverTest).start()
+        time.sleep(2)
+        ans = self.client.connect_to_TCP()
+        self.assertTrue(ans)
+        ans = self.client.close_connection_TCP()
+        self.assertTrue(ans)
+        serverTest.close_connection_TCP()
+
+    def test_close_UDP(self):
+        ans = self.client.connect_to_UDP()
+        self.assertTrue(ans)
 
     def test_data_directory(self):
-        pass
+        path_=path.join(os.getcwd(),"Client\data")
+        shutil.rmtree(path_, ignore_errors=True)
+        ans = path.isdir(path_)
+        self.assertEqual(ans, False)
+        ans = self.client.create_data_directory()
+        self.assertEqual(ans, True)
+        ans = path.isdir(path_)
+        self.assertEqual(ans, True)
 
-    def test_close_connection_TCP(self):
-        pass
+    def test_login(self):
+        
+        db=mongodb("Chat-py")
+        serverTest = TCPServer(db)
+        serverTest.connect_to_TCP()
+        threading.Thread(target=serverTest).start()
+        time.sleep(5)
+        self.client.connect_to_TCP()
+        
+        ans = self.client.login(req="login", user="barak", password="12")
+        self.assertEqual(ans, (False, "Failure"))
+        
+        ans = self.client.login(req="login", user="barak__", password="12")
+        self.assertEqual(ans, (False, "Failure"))
+        
+        ans = self.client.login(req="login", user="barak", password="1")
+        self.assertEqual(ans, (True, "Success"))
 
-    def test_login():
-        pass
+        ans = self.client.login(req="login", user="barak", password="1")
+        self.assertEqual(ans, (False, "Client connected"))
 
-    def test_register():
-        pass
+        self.client.close_connection_TCP()
+        serverTest.close_connection_TCP()
+    
+    def test_register(self):
+        db=mongodb("Chat-py")
+        serverTest = TCPServer(db)
+        serverTest.connect_to_TCP()
+        threading.Thread(target=serverTest).start()
+        self.client.connect_to_TCP()
+        time.sleep(5)
+    
+        ans = self.client.register(req="register", user="barak", password="12")
+        self.assertEqual(ans, (False, "User exist"))
+        
+        ans = self.client.register(req="register", user=randomname.get_name(), password="12")
+        self.assertEqual(ans, (True, "Success"))
+        
+        ans = self.client.register(req="register", user="barak", password="1")
+        self.assertEqual(ans, (False, "User exist"))
 
-    def test_request(self, *args, **kwargs):
-        pass
-
-    def test_response(self):
-        pass
+        self.client.close_connection_TCP()
+        serverTest.close_connection_TCP()
+ 
+    def __del__(self):
+        # self.serverTest.close_connection_TCP()
+        print("Test destroyed, Goodbye!")
+        # exit()
 
 if __name__ == '__main__':
     unittest.main()
--- Test/Test_ServerTCP.py
+++ Test/Test_ServerTCP.py
@@ -1,32 +1,176 @@
+from os import path
+from pydoc import cli
+import sys
+sys.path.append(".")
+
+sys.path.append("Client")
+sys.path.append("Server")
 import unittest
 from Server.api.IRequest import *
+
+import time
+import unittest
+from Server.BLL.TCP_server import TCPServer
+from Client.BLL.Client import Client
+from Server.DAL.DataBase import mongodb
+import threading
+import time
+import randomname
 class TestServer(unittest.TestCase):
 
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName=methodName)
-    def test_write_file(self, *args, **kwargs):
-        pass
+        db=mongodb("Chat-py")
+        self.serverTest = TCPServer(db)
+        self.client=Client(name="barak")
+        
 
-    def test_read_file_log(self, f=0, to=19) -> list[str]:
-        pass
 
     def test_connect_to_TCP(self):
-        pass
+        ans=self.serverTest.connect_to_TCP()
+        self.assertTrue(ans)
+        self.serverTest.close_connection_TCP()
+        
+        self.port=""
+        self.host=""
+        ans=self.serverTest.connect_to_TCP()
+        self.assertFalse(ans)
+        
+        self.serverTest.close_connection_TCP()
 
     def test_close_connection_TCP(self):
-        pass
-
-    def test_registar(self,req:IRequest,connection):
-        pass
-    
-    def test_login(self,req:IRequest,connection):
-        pass
-    
-    def test_send_msg(self:IRequest,req,connection):
-        pass
+        self.serverTest.connect_to_TCP()
+        ans=self.serverTest.close_connection_TCP()
+        self.assertTrue(ans)
         
-    def test_send_msg_to_all(self:IRequest,req,connection):
-        pass
+
+    def test_registar(self):
+        self.serverTest.connect_to_TCP()
+        threading.Thread(target=self.serverTest).start()
+        self.client.connect_to_TCP()
+        time.sleep(5)
+    
+        ans = self.client.register(req="register", user="barak", password="12")
+        self.assertEqual(ans, (False, "User exist"))
+        
+        ans = self.client.register(req="register", user=randomname.get_name(), password="12")
+        self.assertEqual(ans, (True, "Success"))
+        
+        ans = self.client.register(req="register", user="barak", password="1")
+        self.assertEqual(ans, (False, "User exist"))
+
+        self.client.close_connection_TCP()
+        self.serverTest.close_connection_TCP()
+    
+    def test_login(self):
+        self.serverTest.connect_to_TCP()
+        threading.Thread(target=self.serverTest).start()
+        self.client.connect_to_TCP()
+        time.sleep(5)
+        
+        ans = self.client.login(req="login", user="barak", password="12")
+        self.assertEqual(ans, (False, "Failure"))
+        
+        ans = self.client.login(req="login", user="barak", password="1")
+        self.assertEqual(ans, (True, "Success"))
+
+
+        self.client.close_connection_TCP()
+        self.serverTest.close_connection_TCP()
+    
+    def test_send_msg(self):
+        client1=Client(name="sagi")
+        client2=Client(name="barak")
+
+        self.serverTest.connect_to_TCP()
+        threading.Thread(target=self.serverTest).start()
+        client1.connect_to_TCP()
+        client2.connect_to_TCP()
+
+        time.sleep(5)
+        
+        ans = client1.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        ans = client2.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client1.response).start()
+        time.sleep(2)
+       
+        threading.Thread(target=client2.response).start()
+        time.sleep(2)
+        
+        client1.request(req="sendMsg",toClient="barak",msg="hello")
+        
+        
+        client1.close_connection_TCP()
+        client2.close_connection_TCP()
+
+        self.serverTest.close_connection_TCP()
+        
+    def test_send_msg_to_all(self):
+        self.serverTest.connect_to_TCP()
+        threading.Thread(target=self.serverTest).start()
+        
+        client1=Client(name="sagi")
+        client2=Client(name="barak")
+        client3=Client(name="daniel")
+        client4=Client(name="mesi")
+        client5=Client(name="laor")
+
+
+        client1.connect_to_TCP()
+        client2.connect_to_TCP()
+        client3.connect_to_TCP()
+        client4.connect_to_TCP()
+        client5.connect_to_TCP()
+        
+        
+        time.sleep(6)
+        
+        ans = client1.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client1.response).start()
+        time.sleep(2)
+        
+        ans = client2.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client2.response).start()
+        time.sleep(2)
+        
+        ans = client3.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client3.response).start()
+        time.sleep(2)
+        
+        ans = client4.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client4.response).start()
+        time.sleep(2)
+        
+        ans = client5.login(req="login", password="1")
+        self.assertEqual(ans, (True, "Success"))
+        
+        threading.Thread(target=client5.response).start()
+        time.sleep(2)
+        
+        client1.request(req="sendMsgALL",toClient="Friends Group",msg="hello")
+        
+        client1.close_connection_TCP()
+        client2.close_connection_TCP()    
+        client3.close_connection_TCP()
+        client4.close_connection_TCP()  
+        client5.close_connection_TCP()   
+        self.serverTest.close_connection_TCP()
+    
+    def __del__(self):
+        self.serverTest.close_connection_TCP()
+        
 
 if __name__ == '__main__':
     unittest.main()
